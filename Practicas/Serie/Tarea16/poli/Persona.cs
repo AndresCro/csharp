a83ec635f24b6825d7b80dbdebdb00b4c0fe55e5
AndresCro <36737534+AndresCro@users.noreply.github.com>
2020-02-10 15:25:38 -0600

    poli
    bien

---
+++ Practicas/Serie/Tarea16/poli/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*Función
+ * En una orginización se tienen muchas personas con diferentes roles
+ * Cada rol una serie de permisos, así que es necesario indicar que una persona es de tipo persona con rol diferente, empleado, cliente, gerente, persona, lo que se necesite*/
+
+namespace poli
+{
+    class Persona
+    {
+        //Se debe indicar la visibilidad para que sea posible heredar
+        protected string nombre;
+        protected int edad;
+
+        //Método para crear personas
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        
+        //En mi caso voy a sobreescribir el método Datos, por lo tanto es necesrio usar virtual
+        public virtual void Datos()
+        {
+            Console.WriteLine("La persona {0} tiene {1} años",nombre, edad);
+        }
+    }
+
+    class Empleado : Persona
+    {
+        //La nueva variable que la hace diferente
+        int sueldo;
+
+        //Llamo a base para obtener sus atributos
+        public Empleado(string nombre, int edad, int sueldo) : base(nombre, edad)
+        {
+            this.sueldo = sueldo;
+        }
+
+        //Sobreescribo Datos
+        public override void Datos()
+        {
+            Console.WriteLine("La persona {0} tiene {1} años tiene un sueldo de {2}", nombre, edad, sueldo);
+        }
+    }
+}
---
+++ Practicas/Serie/Tarea16/poli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace poli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polimorfismo");
+
+            //Llamo a la misma clase pero con dos diferentes instancias
+            Persona p1 = new Persona("Carlos",25);
+            Persona p2 = new Empleado("Andrés", 20, 50000);
+
+            //Muestro los datos
+            p1.Datos();
+            p2.Datos();
+        }
+    }
+}
