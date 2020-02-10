ca0f0bab5b8795291fa197e291aac02929a2b6f5
AndresCro <36737534+AndresCro@users.noreply.github.com>
2020-02-09 19:51:06 -0600

    tarea10
    bien

---
+++ Practicas/Serie/Tarea10/personas/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personas
+{
+    class Persona
+    {
+        //Variables predeterminadas
+        string nombre;
+        int edad;
+        double estatura;
+        double peso;
+
+        //Constructor necesario de inicialización
+        public Persona(string nombre, int edad, double estatura, double peso)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.estatura = estatura;
+            this.peso = peso;
+        }
+
+        //Una persona puede dibujar, así que un cuadrado se me ocurrió, hace uno 5 x 10 pero en imagen se ve como un cuadrado
+        public void DibujarCuadro()
+        {
+            Console.WriteLine("Dibujo un cuadrado");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    //Si extremos de renglon dibuja --------
+                    if (i == 0 || i == 4)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        //Si son extremos de columna dibuja |
+                        if (j == 0 || j == 9)
+                        {
+                            Console.Write("|");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Calcula el IMC de una persona mediante el peso y la estatura en metros
+        public void Imc()
+        {
+            Console.WriteLine("El IMC de la persona es: " + (peso) / (Math.Pow(estatura, 2)));
+        }
+
+        //Muestra la información de una persona
+        public void Informacion()
+        {
+            Console.WriteLine("Me llamo {0}, tengo {1} años, mido {2:0.00}m y peso {3:0.00}", nombre, edad, estatura, peso);
+        }
+    }
+}
---
+++ Practicas/Serie/Tarea10/personas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Personas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personas");
+
+            //Crea una persona
+            Persona p1 = new Persona("Carlos", 30, 1.80, 70);
+
+            //Muestra lo que puede hacer
+            p1.DibujarCuadro();
+            p1.Informacion();
+            p1.Imc();
+        }
+    }
+}
